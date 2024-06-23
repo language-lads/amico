729b50c73548bcebe02e8da1abcab7dd2af65ca7
Tom Barone <mail@tombarone.net>
2024-06-23 14:08:50 +0930

    Add mailer settings in prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,4 +96,16 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  # Mail setup
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'tombarone.net',
+    user_name: Rails.application.credentials.action_mailer.gmail.username!,
+    password: Rails.application.credentials.action_mailer.gmail.password!,
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 end
