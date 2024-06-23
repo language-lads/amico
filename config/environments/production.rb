8af9721cc8eaade9e2521d69f5f62a6ea0345c5c
Tom Barone <mail@tombarone.net>
2024-06-23 14:48:06 +0930

    Add default url options for the devise mailers

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,6 +98,7 @@ Rails.application.configure do
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 
   # Mail setup
+  config.action_mailer.default_url_options = { host: 'amico.tombarone.net' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
