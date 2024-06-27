aed0a1924b3f536d645c34c78319edfe44fbafb8
Tom Barone <mail@tombarone.net>
2024-06-27 19:05:00 +0930

    Update microphone controller

--- app/javascript/controllers/microphone_controller.js
+++ app/javascript/controllers/microphone_controller.js
@@ -2,42 +2,66 @@ import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   start() {
-    if (!navigator.mediaDevices || !window.MediaRecorder) {
-      console.error(
-        "MediaDevices API or MediaRecorder API not supported in this browser.",
-      );
-      return;
-    }
+    if (isAlreadyRecording()) return;
+    if (!canRecordFromMicrophone()) return;
+
     navigator.mediaDevices
       .getUserMedia({
-        audio: true,
+        audio: {
+          channelCount: {
+            exact: 1,
+          },
+          echoCancellation: {
+            ideal: true,
+          },
+          noiseSuppression: {
+            ideal: true,
+          },
+          autoGainControl: {
+            ideal: true,
+          },
+          voiceIsolation: {
+            ideal: true,
+          },
+          sampleRate: {
+            ideal: 16000, // 16kHz
+          },
+        },
         video: false,
       })
       .then(async (stream) => {
-        console.log("Started recording from the microphone");
-        window.amicoStream = stream;
-        window.amicoStreamContext = new AudioContext();
-        await window.amicoStreamContext.audioWorklet.addModule(
-          "audio-processor.js",
-        );
+        console.debug("Started recording from the microphone");
+        window.amicoStream = stream; // Store globally so we can access it / stop it later
 
-        // Step 4: Connect the node
+        assertOnlyOneAudioTrack(stream);
+        const audioTrack = stream.getAudioTracks()[0];
+        console.debug("Audio track settings: ", audioTrack.getSettings());
+
+        if (!window.amicoStreamContext) {
+          window.amicoStreamContext = new AudioContext({
+            // https://developer.mozilla.org/en-US/docs/Web/API/AudioContext/AudioContext#latencyhint
+            latencyHint: "interactive",
+          });
+          await window.amicoStreamContext.audioWorklet.addModule(
+            "audio-processor.js",
+          );
+        }
         const sampleNode = new AudioWorkletNode(
           window.amicoStreamContext,
           "audio-sample-processor",
         );
-        const source = window.amicoStreamContext.createMediaStreamSource(
-          window.amicoStream,
-        );
+        const source =
+          window.amicoStreamContext.createMediaStreamSource(stream);
         source.connect(sampleNode);
       })
       .catch((error) => {
-        console.error("Could not get user media:", error);
+        stop();
+        console.error("Could not start the microphone", error);
       });
   }
 
   stop() {
-    console.log("Stopped recording from the microphone");
+    console.debug("Stopped recording from the microphone");
     if (window.amicoStream) {
       window.amicoStream.getTracks().forEach((track) => track.stop());
     }
@@ -45,5 +69,38 @@ export default class extends Controller {
       window.amicoStreamContext.close();
       window.amicoStreamContext = null;
     }
+  }
+}
+
+function isAlreadyRecording() {
+  if (window.amicoStream && window.amicoStream.active) {
+    console.debug("Already recording from the microphone");
+    return true;
+  }
+  return false;
+}
+
+function canRecordFromMicrophone() {
+  if (navigator.mediaDevices && window.MediaRecorder) {
+    return true;
+  }
+  console.error(
+    "MediaDevices API or MediaRecorder API not supported in this browser",
+  );
+  return false;
+}
+
+/**
+ * I don't know why there would be more than one audio track...
+ *
+ * @param {MediaStream} stream
+ * @throws {Error}
+ */
+function assertOnlyOneAudioTrack(stream) {
+  const audioTracks = stream.getAudioTracks();
+  if (audioTracks.length != 1) {
+    throw new Error(
+      `Expected exactly one audio track, got ${audioTracks.length}`,
+    );
   }
 }
