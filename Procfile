d3b8150f6919f0cae3c6b9a944633ab9a8bc176a
Tom Barone <mail@tombarone.net>
2024-07-21 16:01:53 +0930

    Add worker process to heroku

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
 release: bundle exec rails db:migrate
-web: GOOD_JOB_EXECUTION_MODE=async bundle exec puma -C config/puma.rb
+web: bundle exec puma -C config/puma.rb
+worker: bundle exec good_job start
--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class ConversationsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_conversation, only: %i[show edit update destroy update_audio]
 
   # GET /conversations or /conversations.json
