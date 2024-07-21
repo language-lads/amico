0135773277912aad63b541668e6eaea2eb23aa89
Tom Barone <mail@tombarone.net>
2024-07-21 16:17:53 +0930

    Add redis caching for the audio samples

--- app/channels/conversation_channel.rb
+++ app/channels/conversation_channel.rb
@@ -8,8 +8,9 @@ class ConversationChannel < ApplicationCable::Channel
 
   def subscribed
     stream_from "conversation_#{params['id']}"
-    @audio_samples = []
     @format = Format.new(:mono, :float, 16_000)
+    @cache = ActiveSupport::Cache::RedisCacheStore.new
+    clear_audio_samples
   end
 
   def unsubscribed
@@ -19,15 +20,30 @@ class ConversationChannel < ApplicationCable::Channel
     Tempfile.create do |f|
       f.binmode
       Writer.new(f, @format) do |writer|
-        writer.write(Buffer.new(@audio_samples, @format))
+        writer.write(Buffer.new(audio_samples, @format))
       end
       conversation.audio.attach(io: File.open(f.path), filename: 'conversation_recording.wav',
                                 content_type: 'audio/wav')
       conversation.update!(status: :completed)
     end
+    clear_audio_samples
   end
 
   def receive(data)
-    @audio_samples.concat(data['audio_samples'])
+    append_audio_samples(data['audio_samples'])
+  end
+
+  private
+
+  def audio_samples
+    @cache.read("conversation_#{params['id']}") || []
+  end
+
+  def append_audio_samples(samples)
+    @cache.write("conversation_#{params['id']}", audio_samples.concat(samples))
+  end
+
+  def clear_audio_samples
+    @cache.write("conversation_#{params['id']}", [])
   end
 end
